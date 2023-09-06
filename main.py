02ac286f3bbc261d04b74ceebe3e016172871e07
Wojslaw <bednarz.wojciech96@gmail.com>
2023-09-06 20:59:26 +0200

    First page modified, added submit button

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import url_for
 from flask import request
 from flask import render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -9,6 +10,14 @@ app = Flask(__name__)
 @app.route('/first_page/')
 def first_page(name=None):
     return render_template('first_page.html', name=name)
+
+
+@app.route("/upload", methods=['POST'])
+def hello():
+    # text = request.form[f"Hello,  {escape(name)}!"]
+    text = request.form["text"]
+    processed_text = text.upper()
+    return render_template('first_page.html', processed_text=processed_text)
 
 
 with app.test_request_context():
